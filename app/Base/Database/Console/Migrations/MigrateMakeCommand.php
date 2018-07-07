9f4bd58341bfa70fe76485c4f74733803037051f
pro-dev-zone <tobychen8782@gmail.com>
2018-07-06 18:37:59 -0700

    Modularize database migrations

---
+++ app/Base/Database/Console/Migrations/MigrateMakeCommand.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Base\Database\Console\Migrations;
+
+use App\Base\App;
+
+class MigrateMakeCommand extends \Illuminate\Database\Console\Migrations\MigrateMakeCommand
+{
+    /**
+     * The console command signature.
+     *
+     * @var string
+     */
+    protected $signature = 'make:migration {module : The module name} {name : The name of the migration.}
+        {--create= : The table to be created.}
+        {--table= : The table to migrate.}
+        {--path= : The location where the migration file should be created.}
+        {--realpath : Indicate any provided migration file paths are pre-resolved absolute paths.}';
+
+    /**
+     * Get migration path (either specified by '--path' option or default location).
+     *
+     * @return string
+     */
+    protected function getMigrationPath()
+    {
+        if (! is_null($targetPath = $this->input->getOption('path'))) {
+            return ! $this->usingRealPath()
+                            ? $this->laravel->basePath().'/'.$targetPath
+                            : $targetPath;
+        }
+        $module = ucfirst(trim($this->argument('module')));
+        if ( ! App::moduleExists($module)) {
+            $this->error('Module: ' . $module . ' doesn\'t exist');
+            die;
+        }
+        $path = App::path('app/' . $module . '/database/migrations');
+        if ( ! file_exists($path)) {
+            mkdir($path, 0755, true);
+        }
+        return $path;
+    }
+}
--- app/Base/Foundation/Providers/ArtisanServiceProvider.php
+++ app/Base/Foundation/Providers/ArtisanServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Base\Foundation\Providers;
 
 use App\Base\Console\ControllerMakeCommand;
+use App\Base\Database\Console\Migrations\MigrateMakeCommand;
 
 class ArtisanServiceProvider extends \Illuminate\Foundation\Providers\ArtisanServiceProvider
 {
@@ -15,6 +16,25 @@ class ArtisanServiceProvider extends \Illuminate\Foundation\Providers\ArtisanSer
     {
         $this->app->singleton('command.controller.make', function ($app) {
             return new ControllerMakeCommand($app['files']);
+        });
+    }
+
+    /**
+     * Register the command.
+     *
+     * @return void
+     */
+    protected function registerMigrateMakeCommand()
+    {
+        $this->app->singleton('command.migrate.make', function ($app) {
+            // Once we have the migration creator registered, we will create the command
+            // and inject the creator. The creator is responsible for the actual file
+            // creation of the migrations, and may be extended by these developers.
+            $creator = $app['migration.creator'];
+
+            $composer = $app['composer'];
+
+            return new MigrateMakeCommand($creator, $composer);
         });
     }
 }
